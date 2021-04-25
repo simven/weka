31a3327be2b1443f01c3699bf006f05bf92041eb
simon_vennat@ens.univ-artois.fr <56834754+simven@users.noreply.github.com>
2021-04-25 23:50:36 +0200

    Update Home.js

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="Home">
           <div className="jumbotron jumbotron-fluid monJumbottron1">
                   <h1 className="display-4">Bienvenue sur Weka !</h1>
-                  <p>Des quiz de différents genres qui vous permettront d'accentuer votre culture tout en vous amusant</p>
+                  <p>Des quiz de différents genres qui vous permettront d'accentuer votre culture tout en vous amusant.</p>
 
                   <div className="lead">
                       <Button variant="primary" href="/quizzes" size="lg">Voir les Quiz</Button>
@@ -22,9 +22,10 @@ export default function Home() {
               <div className="col-6">
                   <h4>Nos Quiz :</h4>
                   <p className="justify">
-                      Premièrement sur notre application vous avez la possibilité de choisir différents thèmes, "chimie" ou "sport", pour votre quiz.
-                      Dans chacun de ces thèmes, vous avez trois questions avec quatre possibilités de réponse.
-                      À la fin de votre quiz, un score vous sera attribué. Aucun point ne vous sera retiré pour chaque mauvaise réponse.
+                      Premièrement, sur notre application, vous avez la possibilité de choisir différents thèmes, "chimie" ou "sport", pour votre quiz.
+                      Dans chacun de ces thèmes, vous avez cinq questions avec quatre possibilités de réponse.
+                      Attention ! Il peut y avoir plusieurs bonnes réponses pour une question !
+                      À la fin de votre quiz, un score vous sera attribué. Aucun point ne vous sera retiré pour une mauvaise réponse.
                   </p>
                   <p className="justify">
                       Maintenant que vous êtes prêt, il ne vous reste plus qu'à vous amuser !
